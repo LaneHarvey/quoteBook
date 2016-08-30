8c41c56f0df8f16beeaee5360c81668ee49d3416
Lane Harvey <mrlaneharvey@gmail.com>
2016-08-30 15:54:39 -0600

    edit

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,3 +1,39 @@
+
 var app = angular.module('quoteBook');
 
 app.controller('mainCtrl', function($scope, dataService) {
+
+  $scope.quotes = dataService.getData();
+
+
+  $scope.addQuote = function() {
+    var quote = buildQuote();
+    dataService.addData(quote);
+    removeQuote();
+  }
+
+  $scope.removeQuote = function() {
+    var quoteToRemove = buildQuote();
+    dataService.removeData(quoteToRemove, $scope.quotes);
+    removeQuote();
+  }
+
+  function buildQuote() {
+    return {
+      text: $scope.text,
+      author: $scope.author
+    }
+  }
+
+  function removeQuote() {
+    $scope.text = '';
+    $scope.author = '';
+  }
+
+  $scope.changeFilter = function() {
+      $scope.filterType = $scope.quoteInput;
+    }
+
+
+
+});
